8411aad32e3deae8b134208b603d22d0b88eb18d
Ayat AbuHaj <abuhajayat@gmail.com>
2022-05-14 08:54:02 +0200

    actions were added to controllers

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,30 @@
 class ApartmentsController < ApplicationController
+  def index
+    @apartments = Apartment.all
+  end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+  end
+
+  def destroy
+    @apartment = Apartment.find(params[:id])
+    @apartment.destroy
+    redirect_to apartments_path
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(:photo, :description, :room, :address, :rent, :size)
+  end
+
 end
