c96400560f5c91888e0bb907960b3fec3132856c
Alex Godfrey <alexgodfrey2@gmail.com>
2022-05-14 15:06:08 +0200

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :apartment do
-    :filter
+  resources :apartments do
+    :filters
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+User.destroy_all
+Apartment.destroy_all
+puts "Database cleaned"
+
+apartment_descriptions = [
+  "Sunny apartment in Kreuzberg with balcony!",
+  "Quaint 1 room apartment near the canal",
+  "Penthouse apartment overlooking beautiful Hermannplatz "
+]
+
+apartment_area = ["Neukolln", "Kreuzberg", "Mitte"]
+
+puts 'creating apartments'
+
+5.times do
+user = User.new(
+  email: "email@email.com",
+  password: "123456"
+)
+user.save!
+
+apartment1 = Apartment.new(
+  description: apartment_descriptions.sample,
+  user_id: user.id,
+  area: apartment_area.sample
+)
+
+file = URI.open("https://images.unsplash.com/photo-1550609531-57c783bb03e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")
+
+apartment1.photos.attach(io: file, filename: file, content_type: "image/jpg")
+apartment1.save!
+end
