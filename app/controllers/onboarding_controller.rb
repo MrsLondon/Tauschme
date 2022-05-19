4c031463a6168c6e2e413b8bd4d0b2dc18128cca
Ayat AbuHaj <abuhajayat@gmail.com>
2022-05-19 21:15:18 +0200

    sign up2

---
+++ app/controllers/onboarding_controller.rb
@@ -0,0 +1,27 @@
+class OnboardingController < ApplicationController
+
+  def new
+    @apartment = Apartment.new
+    @filter = Filter.new
+  end
+
+  def create
+    @apartment = Apartment.new(
+      #image: params["apartment"]["area"],
+      area: params["apartment"]["area"],
+      room: params["apartment"]["room"],
+      rent: params["apartment"]["rent"],
+      description: params["apartment"]["description"],
+    )
+    @apartment.user = current_user
+    @apartment.save
+
+    @filter = Filter.new(
+      area: params["filter"]["area"],
+      room: params["filter"]["room"],
+      rent: params["filter"]["rent"],
+    )
+    @filter.user = current_user
+    @filter.save
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :apartments do
     :filters
   end
+  get "/onboarding", to: "onboarding#new"
+  post "/onboarding", to: "onboarding#create"
+
+  devise_for :users, controllers: { registrations: "registrations" }
 end
