3032e5f1cddd4bad0e8f65319e2e0e8511eb28d6
Alex Godfrey <alexgodfrey2@gmail.com>
2022-05-21 17:41:13 +0200

    random fixes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Controller files must be named *_controller.js.
 
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+import { Application } from "stimulus"
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
