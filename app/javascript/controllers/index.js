6c063a4aa93f1f2d36d23d97fb35190d6bb21c20
Ayat AbuHaj <abuhajayat@gmail.com>
2022-05-21 12:42:30 +0200

    fixing heroku secound attempt

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,8 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-import Carousel from "stimulus-carousel"
+
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
-application.register("carousel", Carousel)
