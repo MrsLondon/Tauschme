2108322190c5a70af2c17f440900b9a982275030
Ali Osmany <ali.osmany@comtravo.com>
2022-05-28 16:42:16 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,109 +30,32 @@ apartment_title = ["Sunny apartment in Berlin", "Altbau", "Neubau", "Groovy Apar
 
 apartment_room = [3, 5, 2, 1, 10]
 
-puts 'Creating users'
-
-user = User.new(
-  email: "email@email.com",
-  password: "123456"
-)
-user.save!
-
-user_two = User.new(
-  email: "usertwo@email.com",
-  password: "123456"
-)
-user_two.save!
-
-puts 'Creating filters'
-
-filter_one = Filter.new(
-  rent: 300,
-  area: "Kreuzberg",
-  room: 3
-)
-
-filter_one.user = user
-filter_one.save!
-
-filter_two = Filter.new(
-  rent: 400,
-  area: "Mitte",
-  room: 2
-)
-
-filter_two.user = user_two
-filter_two.save!
-
-puts 'creating apartments'
-
-apartment1 = Apartment.new(
-  description: apartment_descriptions.sample,
-  user_id: user.id,
-  area: apartment_area.sample,
-
-  rent: apartment_rent.sample,
-
-  title: apartment_title.sample,
-  room: apartment_room.sample,
-
-)
-
-apartment2 = Apartment.new(
-  description: apartment_descriptions.sample,
-  user_id: user.id,
-  area: apartment_area.sample,
-
-  rent: apartment_rent.sample,
-
-  title: apartment_title.sample,
-  room: apartment_room.sample,
-
-)
-
-apartment3 = Apartment.new(
-  description: apartment_descriptions.sample,
-  user_id: user.id,
-  area: apartment_area.sample,
-
-  rent: apartment_rent.sample,
-
-  title: apartment_title.sample,
-  room: apartment_room.sample,
-
-)
-
-apartment4 = Apartment.new(
-  description: apartment_descriptions.sample,
-  user_id: user.id,
-  area: apartment_area.sample,
-
-  rent: apartment_rent.sample,
-
-  title: apartment_title.sample,
-  room: apartment_room.sample,
-
-)
-
-apartment5 = Apartment.new(
-  description: apartment_descriptions.sample,
-  user_id: user.id,
-  area: apartment_area.sample,
-
-  rent: apartment_rent.sample,
-
-  title: apartment_title.sample,
-  room: apartment_room.sample,
-
-)
-
 file = URI.open("https://images.unsplash.com/photo-1550609531-57c783bb03e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80")
-file2 = URI.open("https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80")
 
-apartment1.photos.attach(io: file, filename: file, content_type: "image/jpg")
-apartment1.photos.attach(io: file2, filename: file2, content_type: "image/jpg")
-apartment1.save!
-apartment2.save!
-apartment3.save!
-apartment4.save!
-apartment5.save!
+  puts 'creating users'
+  user = User.new(
+    email: "email@email.com",
+    password: "123456"
+  )
+  user.save!
+
+  user_two = User.new(
+    email: "usertwo@email.com",
+    password: "123456"
+  )
+  user_two.save!
+
+  5.times do
+  puts 'creating apartments'
+  apartment = Apartment.new(
+    description: apartment_descriptions.sample,
+    area: apartment_area.sample,
+    rent: apartment_rent.sample,
+    title: apartment_title.sample,
+    room: apartment_room.sample
+  )
+
+  apartment.user = User.all.sample
+  apartment.save!
+  end
+  Apartment.last.photos.attach(io: file, filename: file, content_type: "image/jpg")
