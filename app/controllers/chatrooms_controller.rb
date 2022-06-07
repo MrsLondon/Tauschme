397d9067d08025b012414ab509e7cfdd0c9785dc
Alex Godfrey <alexgodfrey2@gmail.com>
2022-06-07 20:47:53 +0200

    use /chatroom now as route for navbar matches

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomsController < ApplicationController
+  def index
+    first_chatroom = current_user.matches.first.chatroom
+    redirect_to chatroom_path(first_chatroom)
+  end
+
   def show
     @matches = current_user.matches
     @chatroom = Chatroom.find(params[:id])
