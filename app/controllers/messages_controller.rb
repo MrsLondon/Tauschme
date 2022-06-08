163afe104acab53127e40eaf4efd5a53c554d329
Ayat AbuHaj <abuhajayat@gmail.com>
2022-06-08 12:46:10 +0200

    turbolinks

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,12 @@ class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
-    @message.chatroom = @chatroom
+    @message.chatroom = @room
     @message.user = current_user
     if @message.save
       ActionCable.server.broadcast("everyone",
         render_to_string(partial: 'message', locals: {message: @message} ))
-        redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+        redirect_to chatroom_path(@room, anchor: "message-#{@message.id}")
     else
       render 'chatrooms/show'
     end
@@ -26,7 +26,7 @@ end
 
 
 # if @message.save
-#   ChatroomChannel.broadcast_to(
+#   roomChannel.broadcast_to(
 #     @chatroom,
 #     render_to_string(partial: "message", locals: {message: @message})
 #   )
